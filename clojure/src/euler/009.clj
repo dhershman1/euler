37f10613c14302915dc2bae171a086908d79bc3b
dhershman1 <dustinh17@gmail.com>
2018-12-20 15:50:45 -0500

    Solution 9

---
+++ clojure/src/euler/009.clj
@@ -0,0 +1,23 @@
+; Special Pythagorean Triplet
+; There exists exactly one Pythagorean triplet for a + b + c = 1000
+; Find a, b, and c
+
+(ns euler.009)
+
+(defn sq [x]
+  (* x x))
+
+(defn generate-triple [x]
+  (for [n (range 1 100)
+        m (range n 100)
+        k (range 1 100)
+        :let [a (- (sq m) (sq n))
+              b (* 2 m n)
+              c (+ (sq m) (sq n))
+              d (+ (* k a) (* k b) (* k c))]
+        :when (= d x)]
+    (* a b c)))
+
+(defn -main [& args]
+  (prn
+   (last (generate-triple 1000))))
