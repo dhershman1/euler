0c510461484fb447632a457e4a1b16a2c42b33bb
dhershman1 <dustinh17@gmail.com>
2018-12-20 12:37:14 -0500

    Adding on math dep just in case

--- clojure/project.clj
+++ clojure/project.clj
@@ -3,7 +3,8 @@
   :url "https://github.com/dhershman1/euler"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/math.numeric-tower "0.0.4"]]
   :main ^:skip-aot euler.core
   :target-path "target/%s"
 :profiles {:uberjar {:aot :all}})
