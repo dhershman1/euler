949ff8b3d70cafa21204fa8624170ba23bd74547
dhershman1 <dustinh17@gmail.com>
2018-12-28 16:12:31 -0500

    Clean up clojure solution a little

--- clojure/src/euler/001.clj
+++ clojure/src/euler/001.clj
@@ -2,9 +2,10 @@
 ; Find the sum of all the multiples of 3 or 5 below 1000
 (ns euler.001)
 
+(defn divisible [n]
+  (or (zero? (rem n 3))
+      (zero? (rem n 5))))
+
 (defn -main [& args]
   (prn
-   (reduce + (filter fn [x]
-                     (or (zero? (rem x 3))
-                         (zero? (rem x 5))))
-           (range 1 1000))))
+   (reduce + (filter divisible (range 1 1000)))))
