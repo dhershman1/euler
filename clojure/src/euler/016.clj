158628dfe736df4cb3a66e0eaf0ecdb5b19982d1
dhershman1 <dustinh17@gmail.com>
2019-01-09 08:13:30 -0500

    commit problems 16, and 17

---
+++ clojure/src/euler/016.clj
@@ -0,0 +1,16 @@
+; Power of Digit Sum
+; What is the sum of the digits of the number 2^1000?
+
+(ns euler.016)
+
+(defn pow [n power]
+  (loop [total 1 curr-pow power]
+    (if (= curr-pow 0)
+      total
+      (recur (bigint (* total n)) (dec curr-pow)))))
+
+(defn sum-digits [n]
+  (apply + (map (fn [x] (Character/getNumericValue x)) (str n))))
+
+(defn -main [& args]
+  (time (prn (sum-digits (pow 2 1000)))))
