277f8b5fb6f138a5953832b84f2655d5bb4839a4
dhershman1 <dustinh17@gmail.com>
2019-01-14 12:44:19 -0500

    Format tweak for problem 18

--- clojure/src/euler/018.clj
+++ clojure/src/euler/018.clj
@@ -4,19 +4,19 @@
 (ns euler.018)
 
 (def triangle
-  (map #(Integer/parseInt %)(map #(reduce str %) (partition 2 2 (remove #(or (= \newline %) (= \space %))
-    (seq (slurp "resources/018-triangle.txt")))))))
+  (map #(Integer/parseInt %) (map #(reduce str %) (partition 2 2 (remove #(or (= \newline %) (= \space %))
+                                                                         (seq (slurp "resources/018-triangle.txt")))))))
 
 (def nested-triangle
   (loop [lst triangle n 1 newlist nil]
     (if (empty? lst) (reverse newlist)
-      (recur (drop n lst) (inc n) (cons (take n lst) newlist)))))
+        (recur (drop n lst) (inc n) (cons (take n lst) newlist)))))
 
 (defn max-row [lst]
   (map #(reduce max %) (partition 2 1 lst)))
 
 (defn step-max [lst1 lst2]
-(map + (max-row lst1) lst2))
+  (map + (max-row lst1) lst2))
 
 (defn -main [& args]
   (time (prn (reduce step-max (reverse nested-triangle)))))
